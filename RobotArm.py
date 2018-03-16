7b7753bf8f0a0155eb943fd3fd5bcb9111f48006
dletk <duc.mtle@gmail.com>
2018-03-16 17:57:15 -0500

    Added hand methods, turnExact method, and modify turnRight and turnLeft to be optional wait_until_not_moving

--- RobotArm.py
+++ RobotArm.py
@@ -17,8 +17,8 @@ class RobotArm(object):
 
     # ---------------------------------------------------------------------------
     # The default config that the program will use if no config is given
-    DEFAULT_CONFIG = {COLOR_SENSOR: "in3", BOTTOM_TOUCH_SENSOR: "in1",
-                      TURNING_MOTOR: "outC", VERTICAL_MOVE_MOTOR: "outB", HAND: "outA", GYRO_SENSOR: "in2"}
+    DEFAULT_CONFIG = {COLOR_SENSOR: "in3", BOTTOM_TOUCH_SENSOR: "in2",
+                      TURNING_MOTOR: "outC", VERTICAL_MOVE_MOTOR: "outB", HAND: "outA", GYRO_SENSOR: "in1"}
 
     # ---------------------------------------------------------------------------
 
@@ -67,6 +67,7 @@ class RobotArm(object):
             self.vertical_move_motor = ev3.LargeMotor(port)
         elif motorName == self.HAND:
             self.hand = ev3.MediumMotor(port)
+            self.hand.speed_sp = self.hand.max_speed
         else:
             print("Cannot find the motor name: ", motorName)
 
@@ -150,9 +151,11 @@ class RobotArm(object):
         self.vertical_move_motor.stop_action = "coast"
         self.vertical_move_motor.stop()
 
-    def turnRight(self, speed, time=None):
+    def turnRight(self, speed, time=None, wait_until_not_moving=True):
         """Method to turn the arm to the right with speed from 0 to 1.
         If there is no given time, the arm will turn forever.
+        wait_until_not_moving will make sure the robot stop before doing the next command, set this
+        to False if needed to do some checking while turning
         """
         # In the current setting, a positive speed will make the robot turn right
         speed = self.turning_motor.max_speed * speed
@@ -165,15 +168,75 @@ class RobotArm(object):
             self.turning_motor.run_forever()
         else:
             self.turning_motor.run_timed(time_sp=time * 1000)
-        self.turning_motor.wait_until_not_moving()
+        if wait_until_not_moving:
+            self.turning_motor.wait_until_not_moving()
 
-    def turnLeft(self, speed, time=None):
+    def turnLeft(self, speed, time=None, wait_until_not_moving=True):
         """Method to turn the arm to the left with speed from 0 to 1.
         If there is no given time, the arm will turn forever.
+        wait_until_not_moving will make sure the robot stop before doing the next command, set this
+        to False if needed to do some checking while turning
         """
-        self.turnRight(-speed, time)
+        self.turnRight(-speed, time, wait_until_not_moving)
 
     def turnMotorRelease(self):
         """Method to release the turning motor from holding"""
         self.turning_motor.stop_action = "coast"
         self.turning_motor.stop()
+
+    def handHold(self):
+        """Method to wrap an object"""
+        self.hand.speed_sp = -self.hand.speed_sp
+        self.hand.stop_action = "hold"
+        self.hand.run_timed(time_sp=300)
+
+    def handRelease(self):
+        """Method to release an object from hand"""
+        # Find the current sign of speed in order to reset the speed later
+        current_sign_of_speed = 1 if abs(
+            self.hand.speed_sp) == self.hand.speed_sp else -1
+
+        # Use a slow speed to release the object
+        self.hand.speed_sp = - (self.hand.speed_sp // 10)
+
+        # Make sure the hand is not in hold for too long (getting hot)
+        self.hand.stop_action = "coast"
+        self.hand.run_timed(time_sp=500)
+
+        # Reset the speed with correct sign so handHold can work properly
+        self.hand.speed_sp = current_sign_of_speed * self.hand.max_speed
+
+    def stopTurning(self):
+        """Method to stop the turning_motor"""
+        self.turning_motor.stop()
+        return True
+
+    def turnExact(self, angle, speed=0.02):
+        """Method to turn the arm to left or right with the exact angle
+        If the angle value is positive, turn to the right.
+        If the angle value is negative, tur to the left.
+        """
+        # Make sure there will be no turn if angle = 0
+        if angle == 0:
+            return
+        # Set the current heading to be origin 0
+        self.setHeading()
+
+        # Find out the targeted angle
+        targetHeading = angle if angle > 0 else 360 - abs(angle)
+        print("=====> TARGET HEADING: ", targetHeading)
+        acceptedRange = range(targetHeading - 1, targetHeading + 2)
+        print("=====> ACCEPTED RANGE: ", acceptedRange)
+        if angle < 0:
+            # Initiate the left turn
+            self.turnLeft(speed, wait_until_not_moving=False)
+        else:
+            # Initiate the right turn
+            self.turnRight(speed, wait_until_not_moving=False)
+        while True:
+            # Accept error of 1 degree
+            currentHeading = self.readHeading()
+            print("===> CURRENT HEADING: ", currentHeading)
+            if currentHeading in acceptedRange:
+                self.stopTurning()
+                break
